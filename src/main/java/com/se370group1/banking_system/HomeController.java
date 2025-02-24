aaf89e60235e0a295b3fd4a06723163274972c11
lambo <nathanyflam@gmail.com>
2025-02-24 12:19:35 -0800

    fix controller nameing

--- src/main/java/com/se370group1/banking_system/HomeController.java
+++ src/main/java/com/se370group1/banking_system/HomeController.java
@@ -1,10 +1,10 @@
 package com.se370group1.banking_system;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 
 @Controller
-public class Controller {
+public class HomeController{
 
   @GetMapping("/home")
   public String home() {
